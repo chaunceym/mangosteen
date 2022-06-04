26147d88f724a351680533208c3ee1686c13abad
孟祥宇 <18322276773@163.com>
2022-06-04 13:36:24 +0800

    feat: 第一个欢迎界面

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,14 +1,35 @@
 /*
  * @Author: mengxiangyu
  * @Date: 2022-05-22 17:37:17
- * @LastEditTime: 2022-05-22 17:37:18
+ * @LastEditTime: 2022-06-04 13:33:00
  * @LastEditors: your name
  * @Description:
  */
 
 import { defineComponent } from "vue";
+import s from "./First.module.scss";
+import pig from "../../assets/icons/pig.svg";
+
 export const First = defineComponent({
   setup: (props, context) => {
-    return () => <div>First</div>;
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img class={s.pig} src={pig} alt="pig" />
+          <h2>
+            会挣钱
+            <br />
+            也要会省钱
+          </h2>
+        </div>
+        <div class={s.action}>
+          <RouterLink class={s.fake} to="/welcome/2">
+            跳过
+          </RouterLink>
+          <RouterLink to="/welcome/2">下一页</RouterLink>
+          <RouterLink to="/welcome/2">跳过</RouterLink>
+        </div>
+      </div>
+    );
   },
 });
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,20 +1,25 @@
 /*
  * @Author: mengxiangyu
  * @Date: 2022-05-22 17:37:04
- * @LastEditTime: 2022-06-04 10:39:40
+ * @LastEditTime: 2022-06-04 11:15:59
  * @LastEditors: your name
  * @Description:
  */
 
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
-import s from './Welcome.module.scss';
+import s from "./Welcome.module.scss";
+import logo from "../assets/icons/logo.svg";
 
+console.log(logo);
 export const WelCome = defineComponent({
   setup: (props, context) => {
     return () => (
       <div class={s.wrapper}>
-        <header>logo</header>
+        <header>
+          <img src={logo} alt="logo" />
+          <h1>山竹记账</h1>
+        </header>
         <main>
           <RouterView />
         </main>
